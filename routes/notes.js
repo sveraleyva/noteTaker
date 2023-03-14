1b12884fe913b94635fd3cb98f7b0b386e0e23a5
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-03-14 15:42:26 -0500

    created server, started working on the routes, added helpers folder with sfUtils.js

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
+
+// GET Route
+notes.get("/", (req, res) => {
+  readFromFile("db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const api = require("./routes");
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", api);
+
+// GET route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`app listening at http://localhost:${PORT}`)
+);
