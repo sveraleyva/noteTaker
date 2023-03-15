8bc901bff02dea5f909ddd417858138e70e2c72e
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-03-15 14:30:38 -0500

    updated port const in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes");
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
